79f84578e65c872f35af2c44be41522b8e3a35a5
Josefina Brauning <josefina.brauning@gmail.com>
2025-07-23 02:27:03 -0400

    Add BasicMathModule class skeleton

---
+++ src/BasicMathModule.java
@@ -0,0 +1,19 @@
+
+public class BasicMathModule implements OperationModule {
+
+	@Override
+	public boolean supports(String operation) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public double compute(String operation, double[] operands) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+	
+
+}
