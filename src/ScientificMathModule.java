29dc13b36ea6805adf20a1c587de70fd2d585b53
Josefina Brauning <josefina.brauning@gmail.com>
2025-07-23 02:34:53 -0400

    Add ScientificMathModule class skeleton

---
+++ src/ScientificMathModule.java
@@ -0,0 +1,18 @@
+
+public class ScientificMathModule implements OperationModule{
+
+	@Override
+	public boolean supports(String operation) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public double compute(String operation, double[] operands) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+
+}
