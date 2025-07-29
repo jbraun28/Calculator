a9e445ebec3e4a419909846d52a96e036d05c1b4
Josefina Brauning <josefina.brauning@gmail.com>
2025-07-29 11:46:34 -0400

    define CalculatorController object with ScientificMathModule and BasicMathModule

--- demo/src/main/java/com/example/calculator/CalculatorController.java
+++ demo/src/main/java/com/example/calculator/CalculatorController.java
@@ -5,9 +5,17 @@ import java.util.*;
 @RestController
 @RequestMapping("/api")
 public class CalculatorController {
-
-	private final ScientificMathModule scientificModule = new ScientificMathModule();
-	private final BasicMathModule mathModule = new BasicMathModule();
+	
+	private final List<OperationModule> modules;
+	
+	public CalculatorController() {
+		//create a list of all supported calculator modules
+		this.modules = new ArrayList<OperationModule>();
+		this.modules.add(new BasicMathModule());
+		this.modules.add(new ScientificMathModule());
 		
+	}
+	
+
 
 }
